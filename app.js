66d0782403fa5aac349429c682c2792a8e14ba19
Hamsho <hamidjade@gmail.com>
2018-10-16 17:41:48 +0200

    V. 0 (Boilerplate)
    Added all base-code to have bootstrap ready with CDN assets included and some basic folders for css, js and images.
    
    See readme.md for instructions for your SASS compiler if you are uncertain about what to compile and where to save the compiled output. 
    
    Hint: You can also see in index.html (head) what css is included and WHERE it is supposed to be included from. That is your output path.

---
+++ app.js
@@ -0,0 +1,9 @@
+// Require the express module
+const express = require('express');
+// Create a new web server
+const app = express();
+// Tell the web server to serve files
+// from the www folder
+app.use(express.static('www'));
+// Start the web server on port 3000
+app.listen(3000,() => console.log('Let us get cracking team2! Go do your magic on port 3000'));
