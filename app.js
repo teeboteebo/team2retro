377c2697eaa326cdb6f77a8d68abfb7c4bd9448d
Elias Hjelm <elias.hj@gmail.com>
2018-10-17 16:54:18 +0200

    Included automatic sass compiler in app.js

--- app.js
+++ app.js
@@ -1,3 +1,5 @@
+const Sass = require('./sass');
+const config = require('./config.json');
 // Require the express module
 const express = require('express');
 // Create a new web server
@@ -7,3 +9,7 @@ const app = express();
 app.use(express.static('www'));
 // Start the web server on port 3000
 app.listen(3000,() => console.log('Let us get cracking team2! Go do your magic on port 3000'));
+
+for(let conf of config.sass){
+    new Sass(conf);
+}
